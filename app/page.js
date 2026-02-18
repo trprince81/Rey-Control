dfd4598921e2d2066635f746d3743afb54fd20c4
trprince81 <trprince81@gmail.com>
2026-02-18 18:32:19 -0400

    page.js

--- app/page.js
+++ app/page.js
@@ -1,16 +1,20 @@
+import Link from "next/link"
+
 export default function Home() {
   return (
-    <div style={{ padding: 40, fontFamily: "Arial" }}>
+    <div style={{ padding: 40 }}>
       <h1>👑 Rey Control</h1>
       <hr />
 
       <div style={{ marginTop: 20 }}>
-        <button style={{ padding: 10, marginRight: 10 }}>
-          📦 Inventario
-        </button>
+        <Link href="/inventario">
+          <button style={{ padding: 10, marginRight: 10 }}>
+            🛠️ Lo que has hecho
+          </button>
+        </Link>
 
         <button style={{ padding: 10, marginRight: 10 }}>
-          👥 Clientes
+          💰 Clientes
         </button>
 
         <button style={{ padding: 10, marginRight: 10 }}>
