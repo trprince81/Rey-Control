9d21529e710d8bda5ed56bfa4aaad81bc980cbc7
trprince81 <trprince81@gmail.com>
2026-02-18 16:52:23 -0400

    layout.js

--- app/app/layout.js
+++ app/app/layout.js
@@ -1,23 +1,14 @@
 export const metadata = {
   title: "Rey Control",
-  description: "Sistema de administración",
+  description: "Sistema de administración de clientes",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
-      <body>{children}</body>
-    </html>
-  );
-}export const metadata = {
-  title: "Rey Control",
-  description: "Sistema de administración",
-};
-
-export default function RootLayout({ children }) {
-  return (
-    <html lang="es">
-      <body>{children}</body>
+      <body>
+        {children}
+      </body>
     </html>
   );
 }
