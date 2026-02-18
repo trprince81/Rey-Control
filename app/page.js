700415237ac9730749bde15adaed41b3ca37a287
trprince81 <trprince81@gmail.com>
2026-02-18 17:25:31 -0400

    page.js

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div style={{ padding: 40 }}>
-      <h1>Rey Control 👑</h1>
+      <h1>Rey Control </h1>
       <p>Sistema de administración en construcción...</p>
     </div>
   );
