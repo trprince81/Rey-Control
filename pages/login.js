07b0211d0ad43e193e86164101afe3e3a80491e1
trprince81 <trprince81@gmail.com>
2026-02-19 00:10:25 -0400

    login.js

--- pages/login.js
+++ pages/login.js
@@ -19,27 +19,28 @@ export default function Login() {
     }
   }
 
-  return (
-    <div style={styles.container}>
-      <h1>Imperio S&D</h1>
-
-      <input
-        type="password"
-        placeholder="PIN Admin"
-        value={pin}
-        onChange={(e) => setPin(e.target.value)}
-        style={styles.input}
-      />
-
-      <button style={styles.button} onClick={() => handleLogin("admin")}>
-        Entrar como Admin 👑
-      </button>
-
-      <button style={styles.button} onClick={() => handleLogin("trabajador")}>
-        Entrar como Trabajador 👤
-      </button>
-    </div>
-  );
+  rconst styles = {
+  container: {
+    backgroundColor: "#000",
+    color: "#fff",
+    height: "100vh",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  input: {
+    padding: 10,
+    margin: 10,
+  },
+  button: {
+    padding: 12,
+    margin: 5,
+    backgroundColor: "gold",
+    border: "none",
+    cursor: "pointer",
+  },
+};
 }
 
 const styles = {
