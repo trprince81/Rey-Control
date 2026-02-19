31b69bfe3a268581ba1fa8bc9b9004aa56068f0a
trprince81 <trprince81@gmail.com>
2026-02-18 23:27:57 -0400

    index.js

--- page/index.js
+++ page/index.js
@@ -1,4 +1,12 @@
-pages/index.js
-pages/login.js
-pages/trabajador.js
-pages/admin.js
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
+export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push("/login");
+  }, []);
+
+  return null;
+}
