c9bbdd4316aa0eaf6a110400bf2b56cebef45b43
trprince81 <trprince81@gmail.com>
2026-02-19 20:18:22 -0400

    Update page.jsx

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 import { useRouter } from "next/navigation";
-import { supabase } from "../../lib/supabase";
+import { supabase } from "../../lib/supabaseClient";
 
 export default function Login() {
   const router = useRouter();
