940b4feb0adbf9b614f933fdd42019305f3ad4b7
trprince81 <trprince81@gmail.com>
2026-02-19 20:05:49 -0400

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,1 +1,12 @@
+export const metadata = {
+  title: "Imperio S&D",
+  description: "Sistema",
+};
 
+export default function RootLayout({ children }) {
+  return (
+    <html lang="es">
+      <body>{children}</body>
+    </html>
+  );
+}
